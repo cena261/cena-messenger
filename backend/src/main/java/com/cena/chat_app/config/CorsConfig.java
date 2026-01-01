0b64469fc2b015fce0b93d490fc5af9760a738d0
Cena261 <khanhtu261@gmail.com>
2026-01-01 20:06:34 +0700

    chore: update CORS url

--- backend/src/main/java/com/cena/chat_app/config/CorsConfig.java
+++ backend/src/main/java/com/cena/chat_app/config/CorsConfig.java
@@ -18,8 +18,10 @@ public class CorsConfig {
 
         configuration.setAllowedOrigins(Arrays.asList(
             "http://localhost:5174",
+            "http://localhost:5173",
             "http://localhost:3000",
             "http://127.0.0.1:5174",
+            "http://127.0.0.1:5173",
             "http://127.0.0.1:3000"
         ));
 
