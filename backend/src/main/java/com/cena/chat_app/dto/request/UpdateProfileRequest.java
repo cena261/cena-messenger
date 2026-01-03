423dabf4923515e9e40686e31cd963952e580ca6
Cena261 <khanhtu261@gmail.com>
2026-01-03 15:48:53 +0700

    feat: add profile update request DTOs

---
+++ backend/src/main/java/com/cena/chat_app/dto/request/UpdateProfileRequest.java
@@ -0,0 +1,15 @@
+package com.cena.chat_app.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateProfileRequest {
+    private String displayName;
+    private String avatarUrl;
+}
